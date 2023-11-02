dab0696f72324e45eb97b270543d5744597742ac
ulas96 <y.ulas.yildiz@gmail.com>
2023-11-02 08:37:26 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
+  networks: {
+    goerli: {
+      url: process.env.URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
