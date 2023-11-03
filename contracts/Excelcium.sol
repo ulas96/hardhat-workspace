b2f6f4c20879a4f8595137d05140105907a44e3d
ulas96 <y.ulas.yildiz@gmail.com>
2023-11-03 10:38:37 +0300

    Token update

--- contracts/Excelcium.sol
+++ contracts/Excelcium.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Excelcium is ERC20 {
     uint constant _initial_supply = (10**8) * (10**18);
