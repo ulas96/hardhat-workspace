8ef011a396cf567f94c7b3ca96d466451bfc909c
ulas96 <y.ulas.yildiz@gmail.com>
2023-11-04 01:21:02 +0300

    Bucket.sol created
    It is created to test token transfer to the contract

--- contracts/Bucket.sol
+++ contracts/Bucket.sol
@@ -1,20 +1,40 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 interface IERC20 {
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address account) external view returns (uint);
+
+    function transfer(address recipient, uint amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint amount) external returns (bool);
+
     function transferFrom(
-        address from,
-        address to,
-        uint256 amount
+        address sender,
+        address recipient,
+        uint amount
     ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
 }
+
 
 contract Bucket {
     event Winner(address);
 
-    function drop(address erc20, uint amount) external {
+    address internal excelciumAddress = 0x9e6969254D73Eda498375B079D8bE540FB42fea7;
+
+    IERC20 internal excelcium = IERC20(excelciumAddress);
+
+    function drop(uint amount) external {
         require(amount > 0);
-        require(IERC20(erc20).transferFrom(msg.sender, address(this), amount));
+        require(excelcium.transferFrom(msg.sender, address(this), amount));
         emit Winner(msg.sender);
     }
 }
