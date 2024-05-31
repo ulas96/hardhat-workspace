821cb1c1aa9154bd536edd5782c8b4548da3cade
ulas96 <y.ulas.yildiz@gmail.com>
2024-05-31 13:56:36 +0300

    Create constants.js

---
+++ scripts/constants.js
@@ -0,0 +1,6 @@
+const contractAddr = "0xb3ACAF77c05df37ec32e8628744b173A00b9D6F9";
+const contractName = "PublicCampaigns";
+const personalAddress = "0xfb65f16fBB9f02E4b41c648CAfC99479033eB65A"; //0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 personalAddress = ""; //0x002C65Be429d430DF090f2DC847df3b468676029
+
+
+module.exports = {contractAddr, contractName, personalAddress};
