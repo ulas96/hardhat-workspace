5dafcef1782787910237a333b1472e85ce46d2b4
ulas96 <y.ulas.yildiz@gmail.com>
2024-05-31 13:56:27 +0300

    Update deploy.js
    contract deploy update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,13 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const RPS = await hre.ethers.getContractFactory("RPS");
-  const rps = await RPS.deploy(10);
-  await rps.waitForDeployment();
 
-  console.log(rps.target);
+  const publicCampaigns = await hre.ethers.deployContract("PublicCampaigns");
+
+  await publicCampaigns.waitForDeployment();
+
+  console.log(publicCampaigns.target);
 }
-
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
